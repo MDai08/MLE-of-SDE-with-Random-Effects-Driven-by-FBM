3287c5681ed9c3a3313d71ec63988d9b1d7184b4
MDai08 <64835353+MDai08@users.noreply.github.com>
2020-11-10 11:43:52 +0800

    Add files via upload

---
+++ Example1/H=0.9 mu=-1/N=50 T=8/fBm.m
@@ -0,0 +1,27 @@
+% Generate sample paths for the SDE by fBm
+% dX_t = f(X_t)dt +d1*dB_t^H where B_t^H is fractional brownian motion process
+
+clc;
+clear;
+format long;
+% randn('state',100)
+H = 0.9; 
+M = 2^13; 
+T = 0.001*M;
+dt=T/M;
+t=0:dt:T;
+N = 50;
+X = zeros(M+1,N);
+phi = normrnd(-1,1,[N,1]);
+d = @(x)      1;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+X(1,:)=0;   %initial point
+for i =1:M
+ for j = 1:N
+     [W,t]= fbm1d(H,M,T);
+     X(i+1,j) = X(i,j) + phi(j)*dt + d(X(i,j))*(W(i+1)-W(i));   
+ end
+end
+save X508 X W phi
+plot(t,X,'b-')
+
+
